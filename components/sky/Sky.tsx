dfc6ffd10c5b0dd09c6e3807be22fe4c53114196
Lucas Barbosa <24560842+lbxa@users.noreply.github.com>
2026-08-30 19:06:08 -0400

    new background

--- components/sky/Sky.tsx
+++ components/sky/Sky.tsx
@@ -1,10 +1,3 @@
 export function Sky() {
-  return (
-    <div aria-hidden className="sky-base fixed inset-0 overflow-hidden">
-      <div className="sky-clouds sky-clouds-far" />
-      <div className="sky-clouds sky-clouds-near" />
-      <div className="sky-hill absolute inset-0" />
-      <div className="sky-dither absolute inset-0" />
-    </div>
-  );
+  return <div aria-hidden className="pixel-sky fixed inset-0" />;
 }
